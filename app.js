82cf5ab9290021bfc0d7525acdced30a9a3bd8c3
vraygel <vraygel@mail.ru>
2023-03-26 12:00:20 +0500

    мелкие правки в скрипте

--- app.js
+++ app.js
@@ -19,42 +19,28 @@ let art = document.querySelector('.art')
 const TOKEN = '6234678894:AAEjs2mhQVH72DYnB8s3yFH1_UqnOJDXNFI'
 const CHAT_ID = '-1001836814367'
 const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`
-// const URL_API_foto = `https://api.telegram.org/bot${TOKEN}/sendDocument`
-
-
 
 const TEL_REGEXP = /^(\+7)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
-
 
 function validatePhone(value) {
     return TEL_REGEXP.test(value);
 }
 
-
-
-
 if (document.documentElement.clientWidth <= 768) {
     for (const iterator of anim) {
         iterator.classList.remove('anim')
     }
-    
-    console.log(document.documentElement.clientWidth);
-    
 }
- 
-
 
 for (const iterator of tg) {
     iterator.addEventListener('submit', function (event) {
         event.preventDefault();
 
-
         if (validatePhone(this.tel.value) && this.Name.value != '') {
-
             let message = `<b>Сообщение с сайта innasizova.art</b>\n`
             message += `<b>Имя: ${this.Name.value}</b>\n`
             message += `<b>Телефон: ${this.tel.value}</b>\n`
-            message += `<b>Телефон: ${this.comment.value}</b>\n`
+            message += `<b>Сообщение: ${this.comment.value}</b>\n`
             axios.post(URL_API, {
                 chat_id: CHAT_ID,
                 parse_mode: 'html',
@@ -74,29 +60,21 @@ for (const iterator of tg) {
                 .finally(() => {
                 })
 
-
         } else if (!validatePhone(tel.value)) {
             this.tel.classList.add('fillin')
             let emailTemp = this.tel.value
             this.tel.value = 'не корректный телефон'
 
             setTimeout(() => {
-                // this.tel.classList.remove('fillin')
                 this.tel.value = emailTemp
             }, 3000);
         }
         emailTemp = ''
-
         if (this.Name.value == '') {
             alert('имя не может быть пустым')
-
-
         }
-
     })
 }
-
-
 
 for (const iterator of order) {
     iterator.addEventListener('click', () => {
@@ -115,9 +93,6 @@ image_filter.addEventListener('click', () => {
     image_filter.classList.remove('active')
 })
 
-// let pServices = services.querySelectorAll('p')
-// let hServices = services.querySelectorAll('h1')
-
 for (const iterator of services_item_wrap) {
     iterator.addEventListener('mouseover', () => {
         for (const item of services_item_wrap) {
@@ -134,85 +109,48 @@ for (const iterator of services_item_wrap) {
     })
 }
 
-
-// for (const iterator of help) {
-//     iterator.addEventListener('mouseover', () => {
-//         iterator.classList.remove('help_right')
-//     })
-
-//     iterator.addEventListener('mouseout', () => {
-//         iterator.classList.add('help_right')
-//     })
-// }
-
 try {
-
-
     art.addEventListener('mouseover', () => {
         art.classList.remove('right_art')
     })
-    
     art.addEventListener('mouseout', () => {
         art.classList.add('right_art')
     })
-    
-
     help_art.addEventListener('mouseover', () => {
         help_art.classList.remove('help_art_right')
     })
-
     help_art.addEventListener('mouseout', () => {
         help_art.classList.add('help_art_right')
     })
 
 } catch (err) {
-
     // обработка ошибки
-
 }
-
-
-
-
-
 
 for (const iterator of ideas_item_img) {
     iterator.addEventListener('mouseover', (event) => {
         event.target.previousElementSibling.style.display = 'block';
         event.target.nextElementSibling.style.display = 'block';
-
     })
-
 }
 
 if (shadow_next) {
-    
+
     shadow_next.addEventListener('mouseout', (event) => {
         event.target.parentElement.children[2].style.display = 'none';
         event.target.style.display = 'none';
     })
-
 }
-
-
-
 
 for (const iterator of shadow) {
     iterator.addEventListener('mouseout', (event) => {
         event.target.parentElement.children[2].style.display = 'none';
         event.target.style.display = 'none';
-
     })
-
 }
 
-
-
 let heading_burger = document.querySelector('.heading_burger')
-
-
 let navMobile = document.querySelector('.navMobile')
-
 
 heading_burger.addEventListener('click', () => {
     let classArr = ['up', 'center', 'down']
@@ -223,26 +161,7 @@ heading_burger.addEventListener('click', () => {
 
         element.classList.toggle(`${classArr[index]}`)
         element.classList.toggle(`${classArr_revers[index]}`)
-
     });
-
     navMobile.classList.toggle('downMenu')
     navMobile.classList.toggle('upMenu')
-    
-
 })
-
-
-// for (const iterator of heading_burger.children) {
-//     console.log(iterator.classList); 
-//     // switch (iterator.classList) {
-//     //     case value:
-
-//     //         break;
-
-//     //     default:
-//     //         break;
-//     // }
-
-
-console.log(heading_burger.children);
